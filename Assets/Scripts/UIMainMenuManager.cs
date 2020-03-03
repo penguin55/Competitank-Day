4138aba05a66cb785488e0f1ea0babe372eab210
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-03-03 10:24:58 +0700

    Update Man Menu

---
+++ Assets/Scripts/UIMainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenuManager : MonoBehaviour
+{
+    public void Click()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GAME");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
